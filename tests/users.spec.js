034938d8ccfed4a0a322ad752b75ccf237031c99
SebastianL2 <sebas16cely@gmail.com>
2024-02-05 23:00:12 -0500

    fr

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -2,15 +2,11 @@ import request from "supertest";
 import app from "../app";
 // set NODE_OPTIONS=--experimental-vm-modules npx jest
 
-describe("GET /users", () => {
-    test("should respond with a 200 status code", async () => {
-      const response = await request(app).get("/users").send();
-      expect(response.statusCode).toBe(200);
-    });
-  
-    test("should respond an array", async () => {
-      const response = await request(app).get("/users").send();
-      expect(response.body).toBeInstanceOf(Array);
-    });
+describe('GET /v1/users', () => {
+  test('should respond with a 200 status code', async () => {
+    const response = await request(app).get('/v1/users');
+    expect(response.statusCode).toBe(200);
   });
-  
+
+  // Añade más pruebas según sea necesario para cubrir otros casos de uso
+});
