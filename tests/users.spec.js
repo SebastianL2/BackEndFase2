d51cfc792595c9b9e89bd20465408e83194580e3
SebastianL2 <sebas16cely@gmail.com>
2024-02-06 20:36:55 -0500

    lm

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,12 +1,11 @@
 import request from "supertest";
-import user from "../apiServices/users/route.js";
+import { userRouter } from "../apiServices/users/route.js";
 // set NODE_OPTIONS=--experimental-vm-modules npx jest
 
 describe('GET /v1/users', () => {
   test('should respond with a 200 status code', async () => {
-    const response = await request(app).get('/v1/users');
+    const response = await request(userRouter).get('/v1/users');
     expect(response.statusCode).toBe(200);
   });
 
-  // Añade más pruebas según sea necesario para cubrir otros casos de uso
 });
