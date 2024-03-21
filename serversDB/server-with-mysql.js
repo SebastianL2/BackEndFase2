db7e4eed18a9a6312d14c154f715d987db94e224
SebastianL2 <sebas16cely@gmail.com>
2024-03-21 17:15:58 -0500

    sd

--- serversDB/server-with-mysql.js
+++ serversDB/server-with-mysql.js
@@ -1,5 +1,4 @@
 import { createApp } from "../app.js";
 import { UserModel } from "../modelsDB/mysql/users.js";
-import { VideoModel } from "../modelsDB/MongoDb/videos.js";
 
 createApp ({userModel: UserModel})
