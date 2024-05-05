2ba8fbc2f8034e1a1814fd2b1d0c58b353aacba9
SebastianL2 <sebas16cely@gmail.com>
2024-05-04 23:42:44 -0500

    df

--- modelsDB/mysql/users.js
+++ modelsDB/mysql/users.js
@@ -42,6 +42,7 @@ export class UserModel {
   
       const [uuidResult]= await connection.query('SELECT UUID() uuid;')
       const [{uuid}] = uuidResult
+      console.log("uuid: ", uuid)
       await connection.query(
         `INSERT INTO usersdb (ID, username, email, password, registeredAt, role)
           VALUES (UUID_TO_BIN("${uuid}"),?, ?, ?, ?, ?);`,
